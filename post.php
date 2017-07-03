4fa912665c32928367014b863d172181208f36dd
wshuang <wshuang@yahoo-inc.com>
2017-07-04 00:03:11 +0800

    Refactor the architecture, and use json format as storage.

--- post.php
+++ post.php
@@ -3,14 +3,40 @@ session_start();
 require_once ("./inc/Topic.inc");
 require_once ("./inc/TopicList.inc");
 
-$topicList = new TopicList();
-try {
-    $topic = new Topic($_POST["topic"]);
-} catch (Exception $ex) {
-    echo "Input is over the length limitation";
-}
-echo "123\n";
-$topicList->addTopic($topic);
-$_SESSION['topicList'] = $topicList;
+echo <<< EOF
+<html>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 
+<form action="post.php" method="post">
+    <p>Please input you topic (less than 255 words): </p>
+    <input type="text" name="topic">
+    <input type="submit">
+</form>
+
+</html>
+EOF;
+
+// Processing request
+if (isset($_POST['topic'])) {
+
+    $topicList = new TopicList();
+    if (isset($_SESSION['topicList'])) {
+        $obj = json_decode($_SESSION['topicList'], true);
+        $topicList->setTopicCount($obj['topicCount']);
+        foreach ($obj['topicArray'] as $key) {
+            $topic = Topic::newTopicWithSerial($key['topicName'], $key['topicSerial']);
+            $topicList->addTopic($topic);
+        }
+    }
+
+    $topic = null;
+    try {
+        $topic = Topic::newTopic($_POST["topic"]);
+    } catch (Exception $ex) {
+        echo "Input is over the length limitation";
+    }
+    $topicList->addTopic($topic);
+    $_SESSION['topicList'] = json_encode($topicList);
+    $output = $_SESSION['topicList'];}
+    var_dump($output);
 ?>
