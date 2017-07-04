d90ea327321a0b69edb2cc41009dc67c34aeeee7
wshuang <wshuang@yahoo-inc.com>
2017-07-04 21:33:20 +0800

    add vote js function for every topic

--- index.php
+++ index.php
@@ -4,21 +4,11 @@ require_once ("./inc/Topic.inc");
 require_once ("./inc/TopicList.inc");
 $topicList = new TopicList();
 
-
 echo <<< EOF
-    <script src="vote-js/js/jquery-3.1.0.min.js"></script>
-    <script src="vote-js/lib/jquery.upvote.js"></script>
-    <link rel="stylesheet" href="vote-js/lib/jquery.upvote.css">
-
-<div id="topic-595b5996c347d" class="upvote">
-    <a class="upvote"></a>
-    <span class="count">0</span>
-    <a class="downvote"></a>
-    <a class="star"></a>
-    
-</div>
-
-    <script>
+<script src="vote-js/js/jquery-3.1.0.min.js"></script>
+<script src="vote-js/lib/jquery.upvote.js"></script>
+<link rel="stylesheet" href="vote-js/lib/jquery.upvote.css">
+<script type="text/javascript">
     var callback = function(data) {
     $.ajax({
         url: 'http://d1.maximum.cc/~maximum/reddit/index.php',
@@ -26,28 +16,95 @@ echo <<< EOF
         data: { id: data.id, up: data.upvoted, down: data.downvoted, star: data.starred }
     });
 };
-    $('#topic-595b5996c347d').upvote({id: "595b5996c347d", callback: callback});
-    </script>
+ </script>
 EOF;
+
+
+//echo <<< EOF
+//    <script src="vote-js/js/jquery-3.1.0.min.js"></script>
+//    <script src="vote-js/lib/jquery.upvote.js"></script>
+//    <link rel="stylesheet" href="vote-js/lib/jquery.upvote.css">
+//
+//<div id="topic-595b5996c347d" class="upvote">
+//    <a class="upvote"></a>
+//    <span class="count">0</span>
+//    <a class="downvote"></a>
+//    <a class="star"></a>
+//
+//</div>
+//
+//    <script>
+//    var callback = function(data) {
+//    $.ajax({
+//        url: 'http://d1.maximum.cc/~maximum/reddit/index.php',
+//        type: 'post',
+//        data: { id: data.id, up: data.upvoted, down: data.downvoted, star: data.starred }
+//    });
+//};
+//    $('#topic-595b5996c347d').upvote({id: "595b5996c347d", callback: callback});
+//    </script>
+//EOF;
 
 
 
 
 // Restore data from storage
 if (isset($_SESSION['topicList'])) {
-    $obj = json_decode($_SESSION['topicList'], true);
-    $topicList->setTopicCount($obj['topicCount']);
-    foreach ($obj['topicArray'] as $key => $value) {
-        $topic = Topic::restoreTopic($value['topicName'], $value['totalThumbCount']);
-        $topicList->addTopic($topic, $key);
-    }
+    restoreData($_SESSION['topicList'], $topicList);
 }
-var_dump($topicList);
+
+generateTemplate($topicList);
 
 // vote
 if (isset($_POST['id'])) {
-    $topicId = $_POST['id'];
-error_log("1232323");
+vote($_POST['id'], $topicList);
+}
+
+
+
+function generateTemplate($topicList) {
+    $topicArray = $topicList->getTopicArray();
+    var_dump($topicArray);
+    foreach ($topicArray as $key => $value) {
+        $count = $value->getTotalCount();
+        $topicName = $value->getTopicName();
+        $topicId = "topic-".$key;
+        echo "<div id = $topicId class = \"upvote\">";
+        echo "<p>".$topicName."</p>";
+        echo "<a class=\"upvote\"></a>";
+        echo "<span class=\"count\">".$count."</span>";
+        echo "<a class=\"downvote\"></a>";
+        echo "<script type=\"text/javascript\">";
+        echo "$('#$topicId').upvote({id: \"$key\", callback: callback});";
+//        echo "$('#topic-595b5996c347d').upvote({id: \"595b5996c347d\", callback: callback})";
+        echo "</script>";
+        echo "</div>";
+    }
+
+}
+
+/**
+ * Restore data from json.
+ * @param $data
+ * @param $topicList
+ */
+function restoreData($data, $topicList) {
+        $obj = json_decode($data, true);
+        $topicList->setTopicCount($obj['topicCount']);
+        foreach ($obj['topicArray'] as $key => $value) {
+            $topic = Topic::restoreTopic($value['topicName'], $value['totalThumbCount']);
+            $topicList->addTopic($topic, $key);
+        }
+//        $topicList->getTop20();
+}
+
+
+/**
+ * Vote function
+ * @param $topicId
+ * @param $topicList
+ */
+function vote($topicId, $topicList) {
     $topic = $topicList->getTopicArray();
     if ($_POST['up'] == true) {
         error_log("upup");
@@ -55,19 +112,7 @@ error_log("1232323");
     } elseif ($_POST['down'] == true) {
         $topic[$topicId]->thumbDown();
     }
-
 }
-
-
-//$obj = json_decode($topicList, true);
-//echo "<pre>" . json_encode($obj, JSON_PRETTY_PRINT) . "</pre>";
-//foreach ($obj['topicArray'] as $key => $value) {
-//    echo $key." : ".  $value['topicName']."</br>";
-//}
-
-//foreach ()
-//var_dump($topicList);
-//session_destroy();
 
 // Write to storage
 $_SESSION['topicList'] = json_encode($topicList);
